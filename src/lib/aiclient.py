44705021cbbce597b112cd7557d37ed21a67c331
FateUnix29 <fateofcomputer@proton.me>
2025-07-25 21:14:13 -0700

    Considering if Hollowfire should even be a class.

--- src/lib/aiclient.py
+++ src/lib/aiclient.py
@@ -3,5 +3,22 @@
 class Hollowfire:
     """The main AI client class."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, # pylint: disable=unused-argument
+                 conversation: list[dict[str, str]],
+                 logger,
+                 logger_exit,
+                 stream_handler,
+                 root_dir: str,
+                 system_replacements: dict[str, str],
+                 reset_point: list[dict[str, str]],
+                 memory_dir: str,
+                 profile_dir: str,
+                 startouts_module,
+                 tools_module,
+                 main_module_name: str,
+                 profiles_module,
+                 cli_args,
+                 startout_configuration: int = 0
+                 ):
+
         pass
