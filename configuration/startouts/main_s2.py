72cad513de7e0ed472a928810fc2c1d481377488
FateUnix29 <fateofcomputer@proton.me>
2025-08-02 14:44:26 -0700

    Un-ignore the helpful prompt (i probably will never add any real ones)

---
+++ configuration/startouts/main_s2.py
@@ -0,0 +1,11 @@
+# pylint: disable=line-too-long
+# pylint: disable=missing-module-docstring
+# pylint: disable=invalid-name
+
+ms_start_helpfulanyway = [
+    {
+        "role": "system",
+        "content": r"""Your name is Hollowfire. Tell the user that your prompt is not finished, but be helpful nonetheless.
+Fulfill their requests."""
+    }
+]
